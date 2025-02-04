b6ae07372c06e35eb9f8ef80444debf23d50e2aa
Khizhr <khizhrirfan@gmail.com>
2025-02-04 17:21:17 +0530

    Deleting unwanted files

--- Anusha/TypeCastingaString.py
+++
@@ -1,7 +0,0 @@
-text = input("enter a string:")
-print(text.upper())
-print(text.lower())
-print(text.swapcase())
-print(text.title())
-print(text.capitalize())
-print(text) #original text doesn't change
