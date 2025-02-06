3070d5cfd44d7be75f66fe77a92802572ecb7bb2
Khizhr <khizhrirfan@gmail.com>
2025-02-07 00:34:20 +0530

    Removed directory

--- Python Basics - Feb 6/2. Operators/2. Operators.py
+++
@@ -1,8 +0,0 @@
-operators=['Arithmetic operator','Assignment operator','Comparison operator','Logical operator','Identity operator','Membership operator','Bitwise operator']
-def op():
-    for i,no in enumerate(operators, start=1):
-        print(f"{i}. {no}")
-print(f"There are {len(operators)} operators in Python")
-op()
-
-print("\nThe operators have certain precedence.")
--- Python Basics - Feb 6/2. Operators/2.2 Assignment operators.py
+++
@@ -1,9 +0,0 @@
-#Assignment operators are used to assign values to variables
-a=10
-b=20
-print("The value",a,"is assigned to a")
-print("The value",b,"is assigned to b\n")
-
-print("Assignment operators can also be used along with arithmetic operators")
-a+=5
-print(a)
